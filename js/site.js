ee069f0b8123fee6faad8c4edd996dbcb9d164bf
Rex Lundstrom <rexlundstrom@gmail.com>
2023-04-13 16:33:50 -0400

    logic and basic outline complete

--- js/site.js
+++ js/site.js
@@ -1,10 +1,43 @@
-function displayMessage() {
-  let msg = document.getElementById("message").value;
-  // alert(msg);
+// controller function
+const getValues = () => {
+  let startValue = parseInt(document.getElementById("startValue").value);
+  let endValue = parseInt(document.getElementById("endValue").value);
+  let counter = parseInt(document.getElementById("counter").value);
 
-  Swal.fire({
-    backdrop: false,
-    title: "APP NAME",
-    text: msg,
-  });
-}
+  // error message if NaN
+  if (isNaN(startValue) || isNaN(endValue) || isNaN(counter)) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops!",
+      text: "Enter valid numbers only.",
+      backdrop: false,
+    });
+  } else {
+    const outputStr = generateFizzBuzz(startValue, endValue, counter);
+    displayFizzBuzz(outputStr);
+  }
+};
+
+// business/logic function
+const generateFizzBuzz = (start, end, counter = 100) => {
+  let results = "";
+
+  for (let i = 1; i <= counter; i++) {
+    if (i % start === 0 && i % end === 0) {
+      results += `<td class="fizzbuzz">FizzBuzz</td>`;
+    } else if (i % start === 0) {
+      results += `<td class="fizz">Fizz</td>`;
+    } else if (i % end === 0) {
+      results += `<td class="buzz">Buzz</td>`;
+    } else {
+      results += `<td>${i}</td>`;
+    }
+  }
+  return results;
+};
+
+// view function
+const displayFizzBuzz = (str) => {
+  let tableBody = document.getElementById("results");
+  tableBody.innerHTML = str;
+};
